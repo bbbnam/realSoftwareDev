1210a3be7c7ba4803a2db2325564b9973ad5223e
bbbnam <bbbnam@naver.com>
2021-12-11 20:46:14 +0900

    test : 파서의 유닛 테스트가 실패하는 경우 테스트 코드 작성

---
+++ src/test/java/lowcoupling/BankStatementCSVParserTest.java
@@ -0,0 +1,15 @@
+package lowcoupling;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class BankStatementCSVParserTest {
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        fail("Not yet implemeted");
+    }
+}
