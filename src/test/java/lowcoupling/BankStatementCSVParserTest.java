faf35c3ed3e3acdfb61f63bf8dae077be9101296
bbbnam <bbbnam@naver.com>
2021-12-12 21:20:08 +0900

    test : shouldParseOneCorrectLine 테스트 코드 작성

--- src/test/java/lowcoupling/BankStatementCSVParserTest.java
+++ src/test/java/lowcoupling/BankStatementCSVParserTest.java
@@ -1,15 +1,33 @@
 package lowcoupling;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 class BankStatementCSVParserTest {
     private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void shouldParseOneCorrectLine() throws Exception {
+    public void shouldParseTest() throws Exception {
         fail("Not yet implemeted");
+    }
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        final String line = "30-01-2017,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30),
+                -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        assertThat(expected.getDate()).isEqualTo(result.getDate());
+        assertThat(expected.getAmount()).isEqualTo(result.getAmount());
+        assertThat(expected.getDescription()).isEqualTo(result.getDescription());
     }
 }
